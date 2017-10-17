ff91bcaf2990f23563e04f62e66d5d75ba48b9f3
Ravi Subramanian <scorpiodawg@gmail.com>
2017-10-16 23:33:40 -0700

    Async parsing of manifest is working now

--- test/test-parse.js
+++ test/test-parse.js
@@ -3,12 +3,12 @@ const apkreader = require('../lib/apkreader');
 const process = require('process');
 
 apkreader
-  .load(process.argv.find((i) => i.endsWith("apk")) || '/Users/ravisu/Downloads/com.antutu.ABenchMark.apk')
+  .load(process.argv.find((i) => i.endsWith("apk")) || './test.apk')
   .then((reader) => {
     return reader.readManifest();
   })
   .then((manifest) => {
-    console.log(m);
+    console.log(manifest);
     console.log("Done.");
   })
   .catch((err) => {
